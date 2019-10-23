393b0686b2b2cab49331d24cd57a96196a32e1ed
Alan Ramirez <aramirez2@hudsonhs.com>
2019-10-23 22:15:53 +0000

    only the button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
-
-
+    $("#button").click(function(){
+        let input=
+            $("#input").val();
+    });
   
   
   
