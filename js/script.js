080339375e21467f5aba96097750b79b041c6c20
Alan Ramirez <aramirez2@hudsonhs.com>
2019-11-06 23:30:48 +0000

    i have completed the project but it contains one bug i cant seem to fix where is loads the last word twice.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-
-    $("#button").click(function(){
-        let input=
-            $("#input").val();
+    $("button").click(function(){
+        let intake = $(".input").val();
+            let sentence = intake.split(" ");
+            let ExeptionLetters = ["a", "e", "i", "o", "u"];
+        sentence.forEach(function(word){
+            let FirstL=word[0].toLowerCase();
+                if (ExeptionLetters.includes(FirstL)) {
+                    let word1= word + "ay ";
+                    $(".output").append(word1);
+                }else {
+                    var word2= word.slice(1) + word + "ay ";
+            }
+                                $(".output").append(word2);
+        });
     });
-  
-  
-  
-  
 });
